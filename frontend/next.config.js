1d29c91ad5ae11dbacb8beda8e745b3d38f0bbf7
Lakshya Agarwal <lakshyajannu@gmail.com>
2023-05-27 15:32:24 +0530

    fixed webpack

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    config.externals = ["chrome-aws-lambda"];
 
-module.exports = nextConfig
+    return config;
+  },
+};
+
+module.exports = nextConfig;
